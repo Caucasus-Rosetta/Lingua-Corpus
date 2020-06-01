8b70a29f1e75da59dced5784ef3d791ebbd28005
Nart <daniel.abzakh@gmail.com>
2020-06-01 12:31:55 +0300

    Zip output files

--- scripts/shuffle_ab_ru.py
+++ scripts/shuffle_ab_ru.py
@@ -2,6 +2,7 @@ import io
 import random
 import re
 import sys, getopt
+from zipfile import ZipFile
 inputfile = ["",""]
 outputfile = ['output ab.txt', 'output ru.txt']
 
@@ -26,8 +27,6 @@ def main(argv):
          import pdb; pdb.set_trace()
 count = 0
 def open_parallel_file():
-    out_ab = io.open('../output_shuffled ab.txt','w+', encoding="utf-8")
-    out_ru = io.open('../output_shuffled ru.txt','w+', encoding="utf-8")
     abkhaz_list = []
     in_ab = io.open('../ab.txt','r', encoding="utf-8")
     abkhaz_list.extend(in_ab.readlines())
@@ -43,7 +42,7 @@ def open_parallel_file():
     parallel_corpus = list(dict.fromkeys(parallel_corpus))
     parallel_corpus = sorted(parallel_corpus,key=get_first)
     random.shuffle(parallel_corpus)
-    dirty_sym = re.compile('[0-9-\.№○●:\[\*a-z"▪\?+“\(\)\n]+')
+    dirty_sym = re.compile('[0-9-\.№○●:\[\*a-z"▪\?+“\(\)\n\x0A]+')
     alphabet_ab = re.compile('[ҟцукенгшәзхҿфывапролджҽџчсмитьбҩҵқӷӡҳԥҷҭ]+',re.I)
     alphabet_ru = re.compile('[ёйцукенгшщзхъфывапролджэячсмитьбю]+',re.I)
     def is_dirty(tuple):
@@ -61,13 +60,47 @@ def open_parallel_file():
     for translation_tuple in parallel_corpus:
         if is_dirty(translation_tuple):
             parallel_corpus.remove(translation_tuple)
-    for translation_tuple in parallel_corpus:
-            out_ab.write(translation_tuple[0])
+    f1_text = ""
+    f2_text = ""
+    f3_text = ""
+    f4_text = ""
+    f5_text = ""
+    f6_text = ""   
+    for translation_tuple in parallel_corpus[0:-2000]:
+            f1_text = f1_text + translation_tuple[0]
             if not translation_tuple[0].endswith("\n"):
-                out_ab.write("\n")
-            out_ru.write(translation_tuple[1])
+                f1_text = f1_text + "\n"
+            f4_text = f4_text + translation_tuple[1]
             if not translation_tuple[1].endswith("\n"):
-                out_ru.write("\n")
+                f4_text = f4_text + "\n"
+    for translation_tuple in parallel_corpus[-2000:-500]:
+            f2_text = f2_text + translation_tuple[0]
+            if not translation_tuple[0].endswith("\n"):
+                f2_text = f2_text + "\n"
+            f5_text = f5_text + translation_tuple[1]
+            if not translation_tuple[1].endswith("\n"):
+                f5_text = f5_text + "\n"
+    for translation_tuple in parallel_corpus[-500:-1]:
+            f3_text = f3_text + translation_tuple[0]
+            if not translation_tuple[0].endswith("\n"):
+                f3_text = f3_text + "\n"
+            f6_text = f6_text + translation_tuple[1]
+            if not translation_tuple[1].endswith("\n"):
+                f6_text = f6_text + "\n"
+    archive = ZipFile('ab_ru_'+str(int(len(parallel_corpus)/1000))+'k.zip', 'w')
+    f1 = io.StringIO(f1_text)
+    f2 = io.StringIO(f2_text)
+    f3 = io.StringIO(f3_text)
+    f4 = io.StringIO(f4_text)
+    f5 = io.StringIO(f5_text)
+    f6 = io.StringIO(f6_text)
+    archive.writestr("src-train.txt", f1.getvalue())
+    archive.writestr("src-val.txt", f2.getvalue())
+    archive.writestr("src-test.txt", f3.getvalue())
+    archive.writestr("tgt-train.txt", f4.getvalue())
+    archive.writestr("tgt-val.txt", f5.getvalue())
+    archive.writestr("tgt-test.txt", f6.getvalue())
+    archive.close()
 
 if __name__ == "__main__":
    # main(sys.argv[1:])
