07c2945713ed50f88a9d4d818b687ba498d2de21
Nart <daniel.abzakh@gmail.com>
2020-06-17 17:40:51 +0300

    Refactoring code

--- scripts/shuffle_ab_ru.py
+++ scripts/shuffle_ab_ru.py
@@ -9,158 +9,123 @@ from mosestokenizer import *
 import time
 from tqdm import tqdm
 
-def open_parallel_file():
-    abkhaz_list = []
-    in_ab = io.open('ab.txt','r', encoding="utf-8")
-    abkhaz_list.extend(in_ab.readlines())
+# a funtion to open bitext files as a tupled list
+def open_list(file_tuple):
+    tuple0 = []
+    tuple1 = []
+    with open(file_tuple[0], 'r') as f:
+        tuple0.extend(f.readlines())
+    with open(file_tuple[1], 'r') as f:
+        tuple1.extend(f.readlines())
+    return list(zip(tuple0, tuple1))
 
-    russian_list = []
-    in_ru = io.open('ru.txt','r', encoding="utf-8")
-    russian_list.extend(in_ru.readlines())
-
-    dic_ab_list = []
-    dic_ab = io.open('dictionary.ab','r', encoding="utf-8")
-    dic_ab_list.extend(dic_ab.readlines())
-
-    dic_ru_list = []
-    dic_ru = io.open('dictionary.ru','r', encoding="utf-8")
-    dic_ru_list.extend(dic_ru.readlines())
-
-    para_ab_list = []
-    para_ab = io.open('paraphrases.ab','r', encoding="utf-8")
-    para_ab_list.extend(para_ab.readlines())
-
-    para_ru_list = []
-    para_ru = io.open('paraphrases.ru','r', encoding="utf-8")
-    para_ru_list.extend(para_ru.readlines())
-
-    parallel_corpus = list(zip(abkhaz_list, russian_list))
-    # def get_first(tuple):
-        # return len(tuple[0])
-    # import pdb; pdb.set_trace()
-    # parallel_corpus = sorted(parallel_corpus,key=get_first)
-    parallel_corpus = list(dict.fromkeys(parallel_corpus))
+# a function to clean the text from duplicates and noise
+def clean(corpus):
     dirty_ab = re.compile('[^ҟцукенгшәзхҿфывапролджҽџчсмитьбҩҵқӷӡҳԥҷҭ]+')
     dirty_ru = re.compile('[^ёйцукенгшщзхъфывапролджэячсмитьбю]+')
     alphabet_ab = re.compile('[ҟцукенгшәзхҿфывапролджҽџчсмитьбҩҵқӷӡҳԥҷҭ]+',re.I)
     alphabet_ru = re.compile('[ёйцукенгшщзхъфывапролджэячсмитьбю]+',re.I)
-    def is_dirty(tuple):
-        dirty = False
+    corpus = list(dict.fromkeys(corpus))    
+    temp = list(corpus)
+    print("\nData filtering:")
+    for tuple in tqdm(corpus):
         if (len(dirty_ab.findall(tuple[0])) > 0 and len(alphabet_ab.findall(tuple[0])) == 0) \
         or (len(dirty_ru.findall(tuple[1])) > 0 and len(alphabet_ru.findall(tuple[1])) == 0) \
         or (len(tuple[0])/len(tuple[1]) <= 0.7) \
-        or (len(tuple[0])/len(tuple[1]) >= 1.2):
-            dirty = True
-        return dirty
-    tokenize = MosesTokenizer('ru')
+        or (len(tuple[0])/len(tuple[1]) >= 1.3):
+            temp.remove(tuple)
+    return temp    
+    
+def tokenize_moses(corpus):
+    tokenize = MosesTokenizer('ru')    
+    temp = []    
+    print("\nTokenizing corpus with moses:")
+    for i, tuple in enumerate(tqdm(corpus)):
+        temp.append((tokenize(tuple[0]),tokenize(tuple[1])))
+    return temp
+
+def detokenize_moses(corpus):
     detokenize = MosesDetokenizer('ru')
-    def moses_ab(sent):
-        temp = tokenize(sent)
-        sent = detokenize(temp)
-        return sent + "\n"
-    def moses_ru(sent):
-        temp = tokenize(sent)
-        sent = detokenize(temp)
-        return sent + "\n"
-    temp = list(parallel_corpus)
-    print("\nFiltering data:")
-    for translation_tuple in tqdm(parallel_corpus):
-        if is_dirty(translation_tuple):
-            temp.remove(translation_tuple)
-    parallel_corpus = temp
-    random.shuffle(parallel_corpus)
-    print("-----------------------")
-    archive_ab = ZipFile('ab_ru_'+str(int(len(parallel_corpus)/1000))+'k.zip', 'w', zipfile.ZIP_DEFLATED)
-    archive_ru = ZipFile('ru_ab_'+str(int(len(parallel_corpus)/1000))+'k.zip', 'w', zipfile.ZIP_DEFLATED)
-    print("\nProcessing abkhazian dictionary:")
-    text_dic_ab = ""
-    f_dic_ab = open("ab_sp_dic.txt","w+")
-    for line in tqdm(dic_ab_list):
-        tok = moses_ab(line)
-        text_dic_ab = text_dic_ab + tok
-        f_dic_ab.writelines(tok)
-    print("\nProcessing russian dictionary:")
-    text_dic_ru = ""
-    f_dic_ru = open("ru_sp_dic.txt","w+")
-    for line in tqdm(dic_ru_list):
-        tok = moses_ru(line)
-        text_dic_ru = text_dic_ru + tok
-        f_dic_ru.writelines(tok)
-    print("\nProcessing abkhazian paraphrases:")
-    text_para_ab = ""
-    for line in tqdm(para_ab_list):
-        text_para_ab = text_para_ab + moses_ab(line)
-    print("\nProcessing russian paraphrases:")
-    text_para_ru = ""
-    for line in tqdm(para_ru_list):
-        text_para_ru = text_para_ru + moses_ru(line)
-    f_ab = open("ab_sp_train.txt","w+")
-    f_ru = open("ru_sp_train.txt","w+")
-    for i, translation_tuple in tqdm(enumerate(parallel_corpus)):
-            tok_ab = moses_ab(translation_tuple[0])
-            tok_ru = moses_ru(translation_tuple[1])
-            f_ab.writelines(tok_ab)
-            f_ru.writelines(tok_ru)
-    sp_ab = spm.SentencePieceProcessor()
-    sp_ru = spm.SentencePieceProcessor()
+    temp = []    
+    print("\nDetokenizing corpus with moses:")
+    for i, tuple in enumerate(tqdm(corpus)):
+        temp.append((detokenize(tuple[0]),detokenize(tuple[1])))
+    return temp
+
+# TODO
+def train_sentencepiece(corpus):
     spm.SentencePieceTrainer.Train('--input=ab_sp_train.txt,ab_sp_dic.txt --model_prefix=ab --vocab_size=4000 --model_type=BPE \
                                     --max_sentence_length=20000 --normalization_rule_name=nmt_nfkc_cf \
                                     --character_coverage=1')
-    sp_ab.load("ab.model")
     spm.SentencePieceTrainer.Train('--input=ru_sp_train.txt,ru_sp_dic.txt --model_prefix=ru --vocab_size=4000 --model_type=BPE \
                                     --max_sentence_length=20000 --normalization_rule_name=nmt_nfkc_cf \
                                     --character_coverage=1')
-    sp_ru.load("ru.model")
-    print("\nProcessing training data:")
-    text_ab = ""
-    text_ru = ""
-    for translation_tuple in tqdm(parallel_corpus[0:-2000]):
-            text_ab = text_ab + moses_ab(translation_tuple[0])
-            text_ru = text_ru + moses_ru(translation_tuple[1])
-    ab = io.StringIO(text_dic_ab+text_ab+text_para_ab)
-    ru = io.StringIO(text_dic_ru+text_ru+text_para_ru)
-    archive_ab.writestr("src-train.txt", ab.getvalue())
-    archive_ab.writestr("tgt-train.txt", ru.getvalue())
-    archive_ru.writestr("src-train.txt", ru.getvalue())
-    archive_ru.writestr("tgt-train.txt", ab.getvalue())
-    print("\nProcessing validation data:")
-    text_ab = ""
-    text_ru = ""
-    for translation_tuple in tqdm(parallel_corpus[-2000:-500]):
-            text_ab = text_ab + moses_ab(translation_tuple[0])
-            text_ru = text_ru + moses_ru(translation_tuple[1])
-    ab = io.StringIO(text_ab)
-    ru = io.StringIO(text_ru)
-    archive_ab.writestr("src-val.txt", ab.getvalue())
-    archive_ab.writestr("tgt-val.txt", ru.getvalue())
-    archive_ru.writestr("src-val.txt", ru.getvalue())
-    archive_ru.writestr("tgt-val.txt", ab.getvalue())
-    print("\nProcessing testing data:")
-    text_ab = ""
-    text_ru = ""
-    for translation_tuple in tqdm(parallel_corpus[-500:-1]):
-            text_ab = text_ab + moses_ab(translation_tuple[0])
-            text_ru = text_ru + moses_ru(translation_tuple[1])
-    ab = io.StringIO(text_ab)
-    ru = io.StringIO(text_ru)
-    archive_ab.writestr("src-test.txt", ab.getvalue())
-    archive_ab.writestr("tgt-test.txt", ru.getvalue())
-    archive_ru.writestr("src-test.txt", ru.getvalue())
-    archive_ru.writestr("tgt-test.txt", ab.getvalue())
 
-    archive_ab.write("ab.model")
-    archive_ab.write("ru.model")
-    archive_ru.write("ab.model")
-    archive_ru.write("ru.model")
-    archive_ab.write("ab.vocab")
-    archive_ab.write("ru.vocab")
-    archive_ru.write("ab.vocab")
-    archive_ru.write("ru.vocab")
-    archive_ab.close()
-    archive_ru.close()
+    archive.write("ab.model")
+    archive.write("ru.model")
+    archive.write("ab.vocab")
+    archive.write("ru.vocab")
     os.remove("ab.model")
     os.remove("ru.model")
     os.remove("ab.vocab")
     os.remove("ru.vocab")
 
-# open_parallel_file()
+# TODO
+def tokenize_sentencepiece(corpus):
+    sp_ab = spm.SentencePieceProcessor()
+    sp_ru = spm.SentencePieceProcessor()
+    sp_ab.load("ab.model")
+    sp_ru.load("ru.model")
+
+# TODO
+def detokenize_sentencepiece(corpus):
+    sp_ab = spm.SentencePieceProcessor()
+    sp_ru = spm.SentencePieceProcessor()
+    sp_ab.load("ab.model")
+    sp_ru.load("ru.model")
+
+def zip_data(title,train_list,val_list,test_list):
+    with ZipFile(title, 'w') as archive:
+        with open('src-train.txt','w') as file:
+            for tuple in train_list:
+                file.writelines(tuple[0])
+        archive.write('src-train.txt')
+        with open('tgt-train.txt','w') as file:
+            for tuple in train_list:            
+                file.writelines(tuple[1])
+        archive.write('tgt-train.txt')
+        with open('src-val.txt','w') as file:
+            for tuple in val_list:            
+                file.writelines(tuple[0])
+        archive.write('src-val.txt')
+        with open('tgt-val.txt','w') as file:
+            for tuple in val_list:            
+                file.writelines(tuple[1])
+        archive.write('tgt-val.txt')
+        with open('src-test.txt','w') as file:
+            for tuple in test_list:            
+                file.writelines(tuple[0])
+        archive.write('src-test.txt')
+        with open('tgt-test.txt','w') as file:
+            for tuple in test_list:            
+                file.writelines(tuple[1])
+        archive.write('tgt-test.txt')
+    archive.close()   
+
+def process():
+    file_tuple_list = [("ab.txt","ru.txt"),("dictionary.ab","dictionary.ru"),("paraphrases.ab","paraphrases.ru")]
+    parallel_corpus = open_list(file_tuple_list[0])
+    dictionary_corpus = open_list(file_tuple_list[1])
+    paraphrase_corpus = open_list(file_tuple_list[2])
+#    random.shuffle(clean(parallel_corpus))
+#    parallel_corpus = tokenize_moses(parallel_corpus)
+#    parallel_corpus = detokenize_moses(parallel_corpus)
+#    dictionary_corpus = tokenize_moses(dictionary_corpus)
+#    dictionary_corpus = detokenize_moses(dictionary_corpus)
+#    paraphrase_corpus = tokenize_moses(paraphrase_corpus)
+#    paraphrase_corpus = detokenize_moses(paraphrase_corpus)
+    zip_data('ab_ru_dev.zip',parallel_corpus[0:7000],\
+        parallel_corpus[7000:10000],parallel_corpus[10000:11000])
+    import pdb; pdb.set_trace()
+
+process()
