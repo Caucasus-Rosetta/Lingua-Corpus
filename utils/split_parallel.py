373072b99760143253ebf066d881e392df138d48
Bachstelze <kalle@hilsenbek.de>
2020-07-29 10:35:33 +0200

    bug fix length filtration

--- utils/split_parallel.py
+++ utils/split_parallel.py
@@ -10,21 +10,21 @@ import os
 ab_tokenizer = nltk.data.load("abkhaz_tokenizer.pickle")
 ru_tokenizer = nltk.data.load("russian.pickle")
 
-ab_files = sorted(listdir('../ab'))
-ru_files = sorted(listdir('../ru'))
+ab_files = sorted(listdir('../preprocessed/ab'))
+ru_files = sorted(listdir('../preprocessed/ru'))
 total_match = 0
 total_mismatch = 0
 total_tokenized_sentences = 0
 try:
-    if path.exists('../splitted'):
-        shutil.rmtree('../splitted')
-    os.mkdir('../splitted')
-    if path.exists('../splitted/ab'):
-        shutil.rmtree('../splitted/ab')
-    os.mkdir('../splitted/ab')
-    if path.exists('../splitted/ru'):
-        shutil.rmtree('../splitted/ru')
-    os.mkdir('../splitted/ru')
+    if path.exists('../preprocessed/splitted'):
+        shutil.rmtree('../preprocessed/splitted')
+    os.mkdir('../preprocessed/splitted')
+    if path.exists('../preprocessed/splitted/ab'):
+        shutil.rmtree('../preprocessed/splitted/ab')
+    os.mkdir('../preprocessed/splitted/ab')
+    if path.exists('../preprocessed/splitted/ru'):
+        shutil.rmtree('../preprocessed/splitted/ru')
+    os.mkdir('../preprocessed/splitted/ru')
 except OSError:
     print ("Creation of the directory %s failed" % path)
 
@@ -96,6 +96,9 @@ def correct_sentences(sentences):
         sentences[i] = remHyphen.sub('', sentence)
         sentences[i] = sentence.capitalize()
 
+    if len(sentences) > 0 and sentences[-1] == "":
+        del sentences[-1]
+
     return sentences
 
 
@@ -105,7 +108,7 @@ def split_parallel_list(file_name,parallel_list):
     global total_tokenized_sentences
     mismatched_paragraphs = 0
     matched_paragraphs = 0
-    splitted_list = []
+    preprocessed_list = []
     for i, translation_tuple in enumerate(parallel_list):
         ab_sentences = correct_sentences(ab_tokenizer.tokenize(translation_tuple[0]))
         ru_sentences = correct_sentences(ru_tokenizer.tokenize(translation_tuple[1]))
@@ -113,21 +116,21 @@ def split_parallel_list(file_name,parallel_list):
         if len(ab_sentences) != len(ru_sentences):
             # the paragraphs are mismatched
             mismatched_paragraphs += 1
-            '''
+
             print("\n")
             print(ab_sentences)
             print(ru_sentences)
-            '''
+
         else:
-            # the paragraph should be well splitted
+            # the paragraph should be well preprocessed
             matched_paragraphs += 1
-            splitted_list.extend(list(zip(ab_sentences, ru_sentences)))
+            preprocessed_list.extend(list(zip(ab_sentences, ru_sentences)))
     print(file_name+" mismatched "+str(mismatched_paragraphs)+", matched "+str(matched_paragraphs) \
          +" ("+str(round(mismatched_paragraphs*100/(matched_paragraphs+mismatched_paragraphs)))+"%)")
     total_mismatch+=mismatched_paragraphs
     total_match+=matched_paragraphs
-    total_tokenized_sentences = total_tokenized_sentences + len(splitted_list)
-    return splitted_list
+    total_tokenized_sentences = total_tokenized_sentences + len(preprocessed_list)
+    return preprocessed_list
 
 def open_parallel_file(file_number):
     abkhaz_list = []
@@ -145,13 +148,14 @@ def open_parallel_file(file_number):
 
 print("PARAGRAPHS:")
 for i, file_name in enumerate(ab_files):
+
     parallel_list = open_parallel_file(i)
-    splitted_list = split_parallel_list(file_name, parallel_list)
+    preprocessed_list = split_parallel_list(file_name, parallel_list)
 
-    save_file_ab = io.open('../splitted/ab/'+ab_files[i],'w+', encoding="utf-8")
-    save_file_ru = io.open('../splitted/ru/'+ru_files[i],'w+', encoding="utf-8")
+    save_file_ab = io.open('../preprocessed/splitted/ab/'+ab_files[i],'w+', encoding="utf-8")
+    save_file_ru = io.open('../preprocessed/splitted/ru/'+ru_files[i],'w+', encoding="utf-8")
 
-    for translation_tuple in splitted_list:
+    for translation_tuple in preprocessed_list:
         save_file_ab.write(translation_tuple[0])
         if not translation_tuple[0].endswith("\n"):
             save_file_ab.write("\n")
