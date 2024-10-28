28a3df58979afac689dcd74f7b5114b6b934ee76
Nart <daniel.abzakh@gmail.com>
2024-10-28 20:27:53 +0300

    initial working script for am

--- src/extraction/ady/am.py
+++ src/extraction/ady/am.py
@@ -1,16 +1,38 @@
-import subprocess
-import os
-import glob
+import subprocess, os, glob, fitz, re
+from tqdm import tqdm
 
-def get_git_root():
-    try:
-        root = subprocess.check_output(
-            ["git", "rev-parse", "--show-toplevel"],
-            universal_newlines=True
-        ).strip()
-        return root
-    except subprocess.CalledProcessError:
-        return None
+def root_dir():
+    return subprocess.check_output(["git", "rev-parse", "--show-toplevel"], universal_newlines=True).strip()
+
+def create_file(file_path):
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, 'w', encoding='utf-8') as f: 
+        f.write('')
         
-files = glob.glob(get_git_root()+'/data/raw/ady/2009/*')
-print(files)
+def clean_text(text, replace_chars={'':''}):
+    text = re.sub(r' +', ' ', text)
+    text = re.sub(r'\n\s*\n', '\n', text)
+    for old, new in replace_chars.items():
+       text = re.sub(old, new, text)
+    return text.strip()
+    
+def extract_pdf(pdf_path):
+    doc = fitz.open(pdf_path)
+    text = ""
+    for page in doc:
+        try:
+            fixed_text = page.get_text().encode('latin1').decode('cp1251')
+        except:
+            fixed_text = page.get_text()
+        text += fixed_text + "\n"
+    doc.close()
+    return text
+        
+files = glob.glob(root_dir()+'/data/raw/ady/2009/*')
+output = root_dir()+"/data/interim/ady/1.txt"
+create_file(output)
+with open(output, 'a', encoding='utf-8') as f:
+    for path in tqdm(files):
+        text = extract_pdf(path)
+        text = clean_text(text,{r'-\n':''})
+        f.write(text + '\n')
