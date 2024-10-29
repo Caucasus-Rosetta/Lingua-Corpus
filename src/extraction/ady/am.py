d1d58cdbb3dc9c394877c9be411cbd4969f0857e
Nart <daniel.abzakh@gmail.com>
2024-10-29 17:20:51 +0300

    updated maqa script

--- src/extraction/ady/am.py
+++ src/extraction/ady/am.py
@@ -8,11 +8,26 @@ def create_file(file_path):
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     with open(file_path, 'w', encoding='utf-8') as f: 
         f.write('')
-        
+
+def replace_between_letters(target_letter, replacement, text):
+    pattern = f'(?<=[А-Яа-яЁё]){target_letter}(?=[А-Яа-яЁё])'
+    return re.sub(pattern, replacement, text, flags=re.UNICODE)
+
+def replace_no_letters_before(target_letter, replacement, text):
+    pattern = f'(?<![А-Яа-яЁё]){target_letter}'
+    return re.sub(pattern, replacement, text, flags=re.UNICODE)
+    
+def replace_no_letters_after(target_letter, replacement, text):
+    pattern = f'{target_letter}(?![А-Яа-яЁё])'
+    return re.sub(pattern, replacement, text, flags=re.UNICODE)
+    
 def clean_text(text):
     text = re.sub(r' +', ' ', text)
     text = re.sub(r'\n\s*\n', '\n', text)
     text = re.sub(r'-\n', '', text)
+    text = replace_between_letters('I', 'ӏ', text) # u04CF cyrillic letter small palocka
+    text = replace_no_letters_before('I', 'Ӏ', text) # u04C0 cyrillic letter capital palocka
+    text = replace_no_letters_after('I', 'ӏ', text) # u04CF cyrillic letter small palocka
     return text.strip()
     
 def extract_pdf(pdf_path):
@@ -23,6 +38,16 @@ def extract_pdf(pdf_path):
         text += fixed_text + "\n"
     doc.close()
     return text
+
+def print_unique_chars(filename):
+    unique_chars = set()
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            for char in line:
+                unique_chars.add(char)
+    
+    print("Unique characters in the file:")
+    print(sorted(unique_chars))
         
 files = glob.glob(root_dir()+'/data/raw/ady/2009/*')
 output = root_dir()+"/data/interim/ady/ady/1.txt"
@@ -32,3 +57,5 @@ with open(output, 'a', encoding='utf-8') as f:
         text = extract_pdf(path)
         text = clean_text(text)
         f.write(text + '\n')
+        
+print_unique_chars(output)
