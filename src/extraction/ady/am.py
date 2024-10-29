6932025b057f38babed14a95a54892ba92931ff5
Nart <daniel.abzakh@gmail.com>
2024-10-29 15:19:56 +0300

    edited am script

--- src/extraction/ady/am.py
+++ src/extraction/ady/am.py
@@ -9,30 +9,26 @@ def create_file(file_path):
     with open(file_path, 'w', encoding='utf-8') as f: 
         f.write('')
         
-def clean_text(text, replace_chars={'':''}):
+def clean_text(text):
     text = re.sub(r' +', ' ', text)
     text = re.sub(r'\n\s*\n', '\n', text)
-    for old, new in replace_chars.items():
-       text = re.sub(old, new, text)
+    text = re.sub(r'-\n', '', text)
     return text.strip()
     
 def extract_pdf(pdf_path):
     doc = fitz.open(pdf_path)
     text = ""
     for page in doc:
-        try:
-            fixed_text = page.get_text().encode('latin1').decode('cp1251')
-        except:
-            fixed_text = page.get_text()
+        fixed_text = page.get_text().encode('latin1', errors='ignore').decode('cp1251')
         text += fixed_text + "\n"
     doc.close()
     return text
         
 files = glob.glob(root_dir()+'/data/raw/ady/2009/*')
-output = root_dir()+"/data/interim/ady/1.txt"
+output = root_dir()+"/data/interim/ady/ady/1.txt"
 create_file(output)
 with open(output, 'a', encoding='utf-8') as f:
     for path in tqdm(files):
         text = extract_pdf(path)
-        text = clean_text(text,{r'-\n':''})
+        text = clean_text(text)
         f.write(text + '\n')
